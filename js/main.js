3ed8d9173b1a9fbac9f4f8544ba3af3ff76765df
Aditya-Tiwari-Git <adityat.ipv6@gmail.com>
2026-07-21 01:35:53 +0530

    Changes made to current portfolio to improve it and update it with latest skill

--- js/main.js
+++ js/main.js
@@ -219,47 +219,12 @@
 	/* local validation */
 	$('#contactForm').validate({
 
-		/* submit via ajax */
+		/* submit via static no-key form endpoint */
 		submitHandler: function(form) {
 
 			var sLoader = $('#submit-loader');
-
-			$.ajax({      	
-
-		      type: "POST",
-		      url: "inc/sendEmail.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
-
-		      	sLoader.fadeIn(); 
-
-		      },
-		      success: function(msg) {
-
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
-
-		      },
-		      error: function() {
-
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
-
-		      }
-
-	      });     		
+			sLoader.fadeIn();
+			form.submit();
   		}
 
 	});
